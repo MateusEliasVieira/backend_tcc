1279f6cec1f526c039249cc44b330b90ecc38d74
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-08-22 19:17:50 -0300

    concertando

--- src/main/java/com/equoterapia/dominio/servico/praticante/evolucao/impl/EvolucaoServicoImplementacao.java
+++ src/main/java/com/equoterapia/dominio/servico/praticante/evolucao/impl/EvolucaoServicoImplementacao.java
@@ -55,7 +55,7 @@ public class EvolucaoServicoImplementacao implements EvolucaoServico {
         final Integer[] auxiliar = {0};
         meses.forEach((m) -> {
             evolucaoConsultaFrequenciaProjectionList.forEach((e) -> {
-                if (String.valueOf(e.getMes()+"/"+e.getAno()).equals(m)) {
+                if (String.valueOf(FormataData.verificarMes(e.getMes())+"/"+e.getAno()).equals(m)) {
                     auxiliar[0] += (Integer) e.getFrequencia();
                 }
             });
@@ -76,7 +76,7 @@ public class EvolucaoServicoImplementacao implements EvolucaoServico {
         final Integer[] auxiliar = {0};
         meses.forEach((m) -> {
             evolucaoConsultaFaltasProjectionList.forEach((e) -> {
-                if (String.valueOf(e.getMes()+"/"+e.getAno()).equals(m)) {
+                if (String.valueOf(FormataData.verificarMes(e.getMes())+"/"+e.getAno()).equals(m)) {
                     auxiliar[0] += (Integer) e.getFaltas();
                 }
             });
