d7643266fdb76199c4bbc9eb5d87e62eb67d8200
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-08-28 13:48:47 -0300

    concertando validação de evolução com mesma data

--- src/main/java/com/equoterapia/dominio/repositorio/praticante/evolucao/EvolucaoRepositorio.java
+++ src/main/java/com/equoterapia/dominio/repositorio/praticante/evolucao/EvolucaoRepositorio.java
@@ -16,7 +16,8 @@ public interface EvolucaoRepositorio extends JpaRepository<Evolucao, Long> {
     @Query("select e from Evolucao e where e.praticante.idPraticante = :idPraticante order by e.data desc")
     public Optional<List<Evolucao>> buscarEvolucoesPorChaveEstrangeira(@Param("idPraticante") Long idPraticante);
 
-    @Query("SELECT e FROM Evolucao e WHERE e.praticante.idPraticante = :idPraticante AND FUNCTION('DATE', e.data) = :data")
+//    @Query("SELECT e FROM Evolucao e WHERE e.praticante.idPraticante = :idPraticante AND FUNCTION('DATE', e.data) = :data")
+    @Query("SELECT e FROM Evolucao e WHERE e.praticante.idPraticante = :idPraticante and e.data = :data")
     public Optional<Evolucao> buscarEvolucaoDoPraticantePorData(@Param("idPraticante") Long idPraticante, @Param("data") Date data);
 
     @Query("SELECT e FROM Evolucao e WHERE e.data BETWEEN :dataInicial AND :dataFinal AND e.praticante.idPraticante = :idPraticante")
